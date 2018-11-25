cc9287874859e4c3ccdfa112a751568bea1bac93
Алексей Гардер <aleksey_garder@outlook.com>
2018-11-25 12:57:56 +0700

    Add optimization

--- CudaProj2/kernel.cu
+++ CudaProj2/kernel.cu
@@ -7,16 +7,7 @@
 #include <forward_list>
 #include <chrono>
 
-
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-cudaError_t findSimpleDividersWithCUDA(std::forward_list<long> *result, long long value, int cudaCores);
-
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
-}
-
+cudaError_t findSimpleDividersWithCUDA(std::forward_list<long long> *result, long long value, int cudaCores);
 
 __device__ bool isPrime(long long value)
 {
@@ -28,24 +19,25 @@ __device__ bool isPrime(long long value)
 	return true;
 }
 
-__global__ void getSimpleDividersKernel(char *output, long long from, long long value)
+__global__ void getSimpleDividersKernel(char *output, long long from, long long value, int step)
 {
-	cudaError_t status;
+	const long long currentDefault = threadIdx.x + from;
 
-	unsigned int i = threadIdx.x;
-	long long current = i + from;
-
-	output[i] = 0;
-
-	if (value%current != 0 || !isPrime(current))
+	for (long long i = currentDefault; i <= value; i+=step)
 	{
-		return;
-	}
+		long long tempVal = value;
+		long long outPos = i - from;
 
-	while (value%current == 0)
-	{
-		output[i]++;
-		value /= current;
+		output[outPos] = 0;
+
+		if (tempVal%i == 0 && isPrime(i))
+		{
+			while (tempVal%i == 0)
+			{
+				output[outPos]++;
+				tempVal /= i;
+			}
+		}
 	}
 }
 
@@ -55,8 +47,8 @@ int main()
 	using namespace std;
 
 	char *outputArray = nullptr;
-	int value;
-	std::forward_list<long> result;
+	long long value;
+	std::forward_list<long long> result;
 
 	std::cout << "Write value:" << std::endl;
 	std::cin >> value;
@@ -86,97 +78,21 @@ int main()
 
 	for (auto item : result)
 	{
-		std::cout << (int) item << ' ';
+		std::cout << (long long) item << ' ';
 	}
 
     return 0;
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
+cudaError_t findSimpleDividersWithCUDA(std::forward_list<long long> *result, long long value, int cudaCores)
 {
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
+	const long long from = 2;
+	const long long buferSize = value - from;
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
-}
-
-cudaError_t findSimpleDividersWithCUDA(std::forward_list<long> *result, long long value, int cudaCores)
-{
-	const int from = 2;
-	const int buferSize = value > cudaCores ? cudaCores : value - from;
-	const int divCount = (value - from) / buferSize + ((value-from)%buferSize == 0 ? 0 : 1 );
+	if (buferSize < cudaCores)
+	{
+		cudaCores = buferSize;
+	}
 
 	char *buffer_output = new char[buferSize];
 	char *dev_output;
@@ -198,42 +114,36 @@ cudaError_t findSimpleDividersWithCUDA(std::forward_list<long> *result, long lon
 		goto Error;
 	}
 
-	for (int i = 0; i < divCount; i++)
+
+	getSimpleDividersKernel <<<1, cudaCores >>> (dev_output, from, value, cudaCores);
+
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		goto Error;
+	}
+
+	// cudaDeviceSynchronize waits for the kernel to finish, and returns
+	// any errors encountered during the launch.
+	cudaStatus = cudaDeviceSynchronize();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+		goto Error;
+	}
+
+	cudaStatus = cudaMemcpy(buffer_output, dev_output, buferSize, cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy 260 failed!");
+		goto Error;
+	}
+
+	for (long long j = 0; j < buferSize; j++)
 	{
-		long long start = i * buferSize + from;
-		int taskCount = (i == divCount - 1) ? value - start : buferSize;
-		
-		
-		getSimpleDividersKernel << <1, taskCount >> > (dev_output, start, value);
-
-		cudaStatus = cudaGetLastError();
-		if (cudaStatus != cudaSuccess) {
-			fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-			goto Error;
-		}
-
-		// cudaDeviceSynchronize waits for the kernel to finish, and returns
-		// any errors encountered during the launch.
-		cudaStatus = cudaDeviceSynchronize();
-		if (cudaStatus != cudaSuccess) {
-			fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-			goto Error;
-		}
-
-		cudaStatus = cudaMemcpy(buffer_output, dev_output, buferSize, cudaMemcpyDeviceToHost);
-		if (cudaStatus != cudaSuccess) {
-			fprintf(stderr, "cudaMemcpy 260 failed!");
-			goto Error;
-		}
-
-		for (int j = 0; j < taskCount; j++)
+		int itCount = (int)buffer_output[j];
+		for (int k = 0; k < itCount; k++)
 		{
-			int itCount = (int)buffer_output[j];
-			for (int k = 0; k < itCount; k++)
-			{
-				long long tempValue = start + j;
-				result->push_front(tempValue);
-			}
+			long long tempValue = from + j;
+			result->push_front(tempValue);
 		}
 	}
 
